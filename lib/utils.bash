6c2746bc79910a00a74bc968bc9a03ac263bc375
mrWinston <maschulz@redhat.com>
2023-05-05 11:16:04 +0200

    fix install path

--- lib/utils.bash
+++ lib/utils.bash
@@ -55,7 +55,7 @@ download_release() {
 install_version() {
   local install_type="$1"
   local version="$2"
-  local install_path="${3%/bin}/bin"
+  local install_path="${3%/bin}"
 
   if [ "$install_type" != "version" ]; then
     fail "asdf-$TOOL_NAME supports release installs only"
